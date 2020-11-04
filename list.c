92d6e273e8b47acb2fba0965672b95053ef387cd
James Sergeant <57397095+James-Sergeant@users.noreply.github.com>
2020-11-04 10:59:58 +0000

    IT WORKS!

--- list.c
+++ list.c
@@ -5,27 +5,38 @@ typedef struct listStruct{
 
 
 List* setupList(List* stringList, char* string){
+    char * binPath = "/bin/";
+    char* commandPath= malloc(256*sizeof(char));
+    strcpy(commandPath,binPath);
+    strcat(commandPath,string);
+    //printf("%s",commandPath);
    stringList->list = (int**)malloc(sizeof(int*));
    stringList->length = 1;
-   stringList->list[stringList->length - 1] = string;
+   stringList->list[stringList->length - 1] =commandPath;
 
 
 
     return stringList;
 }
 
-List* addString(List* stringList, char* string){
+List* addArgs(List* stringList, char* string){
         stringList->list = realloc(stringList->list, sizeof(int *) * ++stringList->length);
         stringList->list[stringList->length - 1] = string;
     return stringList;
 }
-
-char* getString(List* stringList ,int x){
+char* getElement(List* stringList , int x){
     return stringList->list[x];
 }
-
 void printList(List* list){
     for(int i = 0; i < list->length; i++){
-        printf("%s\n", getString(list, i));
+        printf("%s\n", getElement(list, i));
     }
 }
+int** getCommandList(List* list){
+    addArgs(list, NULL);
+    //printList(list);
+    return list->list;
+}
+
+
+
